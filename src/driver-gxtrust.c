5661d093aa333507b67f451f63803889457d3734
psysrc <sam.copping@yahoo.com>
2025-05-10 14:09:35 +0100

    Hello Helox Mouse - minimum working example to detect the mouse

--- src/driver-gxtrust.c
+++ src/driver-gxtrust.c
@@ -9,6 +9,22 @@ gxtrust_probe(struct ratbag_device *device)
 		device->ids.vendor, device->ids.product,
 		device->name);
 
+	// Create 1 profile, 1 resolution, 1 button, 0 LEDs
+	ratbag_device_init_profiles(device, 1, 1, 1, 0);
+
+	struct ratbag_profile *profile = ratbag_device_get_profile(device, 0);
+	profile->is_active = true;
+
+	struct ratbag_resolution *res = ratbag_profile_get_resolution(profile, 0);
+    res->is_active = true;
+
+    // Set dummy DPI range
+    ratbag_resolution_set_dpi_list_from_range(res, 100, 2000);
+    ratbag_resolution_set_resolution(res, 800, 800);
+
+	unsigned int rates[] = { 500, 1000 };
+	ratbag_profile_set_report_rate_list(profile, rates, ARRAY_LENGTH(rates));
+
     return RATBAG_SUCCESS;
 }
 
